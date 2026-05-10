fd7bfd192efbf9c43a242220e6847fe928f39ddb
Devmallya777 <devmallyachakraborty456@gmail.com>
2026-05-10 17:21:15 +0530

    Full premium website redesign

--- server.js
+++ server.js
@@ -29,6 +29,28 @@ const transporter = nodemailer.createTransport({
 });
 
 /* =========================
+   TRANSPORTER HERE
+========================= */
+
+const transporter = nodemailer.createTransport({
+
+    host: "smtp.gmail.com",
+
+    port: 465,
+
+    secure: true,
+
+    auth: {
+
+        user: process.env.EMAIL_USER,
+
+        pass: process.env.EMAIL_PASS
+
+    }
+
+});
+
+/* =========================
    CONTACT API
 ========================= */
 
