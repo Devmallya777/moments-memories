536a0434fc2e291263c4774377b9f8584d0d00e6
Devmallya777 <devmallyachakraborty456@gmail.com>
2026-05-10 13:34:17 +0530

    fixed user model path

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const otpGenerator = require('otp-generator');
 const nodemailer = require('nodemailer');
 const jwt = require('jsonwebtoken');
 
-const User = require("../models/User");
+const User = require("../models/user");
 
 const router = express.Router();
 
