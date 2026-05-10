8e8741891cd7ada90f47e423b632508327b68abb
Devmallya777 <devmallyachakraborty456@gmail.com>
2026-05-10 22:31:41 +0530

    new update

--- models/Product.js
+++ models/Product.js
@@ -3,11 +3,8 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
 
     title: String,
-
     price: Number,
-
     image: String,
-
     description: String
 
 });
--- server.js
+++ server.js
@@ -12,29 +12,61 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 mongoose.connect(process.env.MONGO_URI)
-
 .then(() => {
-
     console.log("MongoDB Connected");
-
 })
-
 .catch((err) => {
-
     console.log(err);
-
 });
 
+const Product = require("./models/Product");
+const Contact = require("./models/Contact");
+
 app.get("/", (req, res) => {
-
     res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
+app.get("/api/products", async (req, res) => {
+    const products = await Product.find();
+    res.json(products);
+});
+
+app.post("/api/products", async (req, res) => {
+
+    const { title, price, image, description } = req.body;
+
+    const product = new Product({
+        title,
+        price,
+        image,
+        description
+    });
+
+    await product.save();
+
+    res.json({ success: true });
+});
+
+app.post("/api/contact", async (req, res) => {
+
+    const { name, email, message } = req.body;
+
+    const contact = new Contact({
+        name,
+        email,
+        message
+    });
+
+    await contact.save();
+
+    res.json({
+        success: true,
+        message: "Message Saved"
+    });
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-
     console.log(`Server running on port ${PORT}`);
-
 });
